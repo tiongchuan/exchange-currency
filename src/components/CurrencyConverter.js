f0c2bd3c7e9e6e4034d8f0d7e94157482e00192f
tiongchuan <tiongchuan.ongtc@gmail.com>
2022-06-10 01:03:27 +0800

    added select currencies

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -7,10 +7,37 @@ const CurrencyConverter = () => {
     const showDate = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`
 
     const [amount, setAmount] = useState(1);
+    const [input, setInput] = useState("SGD");
+    const [output, setOutput] = useState("USD");
+    const [currencies, setCurrencies] = useState([]);
 
     const handleAmount = (e) => {
         setAmount(e.target.value)
     }
+
+    const handleInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleOutput = (e) => {
+        setOutput(e.target.value)
+    }
+
+    const GetCurrencies = async () => {
+        const { request, data } = await API.get("/latest")
+
+        if (request.status === 200) {
+            console.log(data);
+            const changeCurrencyArray = Object.keys(data.rates)
+            console.log(changeCurrencyArray);
+            setCurrencies(changeCurrencyArray)
+            console.log(currencies);
+        }
+    }
+
+    useEffect(() => {
+        GetCurrencies()
+    }, [])
 
     return (
         <>
@@ -23,6 +50,16 @@ const CurrencyConverter = () => {
 
                 <div className="form">
                     <input type="nunber" value={amount} onChange={handleAmount}></input>
+                    <select value={input} onChange={handleInput}>
+                        {currencies.map((o) =>
+                            <option key={o}>{o}</option>
+                        )}
+                    </select>
+                    <select value={output} onChange={handleOutput}>
+                        {currencies.map((o) =>
+                            <option key={o}>{o}</option>
+                        )}
+                    </select>
                 </div>
 
 
