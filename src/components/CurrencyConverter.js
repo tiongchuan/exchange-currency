26befba34e6fd355894e60d1d7b8c31c0514a379
tiongchuan <tiongchuan.ongtc@gmail.com>
2022-07-22 01:03:04 +0800

    some changes

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import API from "../API";
-import CurrencyChart from "./CurrencyChart"
+import CurrencyChart from "./CurrencyChart.js"
 import './CurrencyConverter.css'
 
 const CurrencyConverter = () => {
