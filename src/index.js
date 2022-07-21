eb5c598c86366150916836d53ddaa192fe59670c
tiongchuan <tiongchuan.ongtc@gmail.com>
2022-07-22 01:04:37 +0800

    some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import CurrencyConverter from './components/CurrencyConverter';
+import CurrencyConverter from './components/CurrencyConverter.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
